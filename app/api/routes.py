87a4fea8cbbe86f866d3bdfe4b2870efe66985ac
zsinx6 <zsinx6@users.noreply.github.com>
2020-09-22 03:15:19 -0300

    Add REST API files

---
+++ app/api/routes.py
@@ -0,0 +1,39 @@
+from flask import abort, jsonify
+from flask_restful import Resource, reqparse
+from sqlalchemy.exc import IntegrityError
+
+from app import rest_api, db
+from app.api.auth import token_auth
+from app.models import User
+
+
+class new_user(Resource):
+    method_decorators = [token_auth.login_required]
+    parser = reqparse.RequestParser()
+    parser.add_argument("username", type=str)
+    parser.add_argument("twitch_id", type=int)
+    parser.add_argument("profile_image_url", type=str)
+
+    def post(self):
+        document = self.parser.parse_args(strict=True)
+        username = document.get("username")
+        twitch_id = document.get("twitch_id")
+        profile_image_url = document.get("profile_image_url")
+
+        user = User(username=username, twitch_id=twitch_id, profile_image_url=profile_image_url)
+        db.session.add(user)
+        try:
+            db.session.commit()
+        except IntegrityError as ex:
+            abort(400, message=str(ex))
+
+        json_send = {}
+        json_send[user.id] = {
+            "username": username,
+            "twitch_id": twitch_id,
+            "profile_image_url": profile_image_url,
+        }
+        return jsonify(json_send)
+
+
+rest_api.add_resource(new_user, "/new_user")
