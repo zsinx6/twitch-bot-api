81f93e74c524a005a43570c76ac96394361b2268
zsinx6 <zsinx6@users.noreply.github.com>
2020-09-22 03:13:39 -0300

    Create model for LoginUser

--- app/models.py
+++ app/models.py
@@ -1,4 +1,10 @@
-from datetime import datetime
+import base64
+import os
+from datetime import datetime, timedelta
+
+from flask_login import UserMixin
+from werkzeug.security import check_password_hash, generate_password_hash
+
 from app import db
 
 
@@ -40,3 +46,44 @@ class OnlineAlert(db.Model):
 
     def __repr__(self):
         return f"OnlineAlert {self.message_id}"
+
+
+class LoginUser(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+    token = db.Column(db.String(32), index=True, unique=True)
+    token_expiration = db.Column(db.DateTime)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def get_token(self, expires_in=3600):
+        now = datetime.utcnow()
+        if self.token and self.token_expiration > now + timedelta(seconds=60):
+            return self.token
+        self.token = base64.b64encode(os.urandom(24)).decode("utf-8")
+        self.token_expiration = now + timedelta(seconds=expires_in)
+        db.session.add(self)
+        return self.token
+
+    @staticmethod
+    def check_token(token):
+        user = LoginUser.query.filter_by(token=token).first()
+        if user is None or user.token_expiration < datetime.utcnow():
+            return None
+        return user
+
+    def to_dict(self):
+        data = {"id": self.id, "username": self.username}
+        return data
+
+    def from_dict(self, data, new_user=False):
+        for field in ["username"]:
+            if field in data:
+                setattr(self, field, data[field])
+        if new_user and "password" in data:
+            self.set_password(data["password"])
