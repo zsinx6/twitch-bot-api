4d272e92fd0a10024953f298e44bdad10a6ff553
zsinx6 <zsinx6@users.noreply.github.com>
2020-09-23 11:19:13 -0300

    Improve Alert post method

--- app/api/resources.py
+++ app/api/resources.py
@@ -1,24 +1,41 @@
-from flask import abort, jsonify
-from flask_restful import Resource, reqparse
+from flask import jsonify, request
+from flask_restful import Resource, abort
 from sqlalchemy.exc import IntegrityError
 
-from app import db, rest_api
+from app import db
 from app.api.auth import token_auth
-from app.models import User
+from app.models import Alert, User
 
 
-class new_user(Resource):
+class Alerts(Resource):
     method_decorators = [token_auth.login_required]
-    parser = reqparse.RequestParser()
-    parser.add_argument("username", type=str)
-    parser.add_argument("twitch_id", type=int)
-    parser.add_argument("profile_image_url", type=str)
 
     def post(self):
-        document = self.parser.parse_args(strict=True)
+        document = request.get_json()
+        user = self._create_user(document)
+        alert = self._create_alert(document, user)
+
+        json_send = {}
+        json_send[alert.id] = {
+            "username": user.username,
+            "twitch_id": user.twitch_id,
+            "profile_image_url": user.profile_image_url,
+            "channel_id": alert.channel_id,
+            "message_text": alert.message_text,
+        }
+        return jsonify(json_send)
+
+    def _create_user(self, document):
         username = document.get("username")
         twitch_id = document.get("twitch_id")
         profile_image_url = document.get("profile_image_url")
+
+        if not all((username, twitch_id, profile_image_url)):
+            abort(400, message="username, twitch_id and profile_image_url should not be empty")
+
+        user = User.query.filter_by(username=username, twitch_id=twitch_id).first()
+        if user:
+            return user
 
         user = User(username=username, twitch_id=twitch_id, profile_image_url=profile_image_url)
         db.session.add(user)
@@ -26,14 +43,19 @@ class new_user(Resource):
             db.session.commit()
         except IntegrityError as ex:
             abort(400, message=str(ex))
+        return user
 
-        json_send = {}
-        json_send[user.id] = {
-            "username": username,
-            "twitch_id": twitch_id,
-            "profile_image_url": profile_image_url,
-        }
-        return jsonify(json_send)
+    def _create_alert(self, document, user):
+        channel_id = document.get("channel_id")
+        message_text = document.get("message_text")
 
+        if not all((channel_id, message_text)):
+            abort(400, message="channel_id and message_text should not be empty")
 
-rest_api.add_resource(new_user, "/new_user")
+        alert = Alert(channel_id=channel_id, message_text=message_text, user=user)
+        db.session.add(alert)
+        try:
+            db.session.commit()
+        except IntegrityError as ex:
+            abort(400, message=str(ex))
+        return alert
