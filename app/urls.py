5e7436f0ed6f5d2787177503befe19e2a3c6a0da
zsinx6 <zsinx6@users.noreply.github.com>
2020-09-23 23:13:12 -0300

    Add /tokens to urls file

--- app/urls.py
+++ app/urls.py
@@ -1,4 +1,5 @@
 from app import rest_api
-from app.api.resources import Alerts
+from app.api.resources import Alerts, Tokens
 
 rest_api.add_resource(Alerts, "/alerts")
+rest_api.add_resource(Tokens, "/tokens")
