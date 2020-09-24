532e975f765bb0650df664755d10b230d3cfba60
zsinx6 <zsinx6@users.noreply.github.com>
2020-09-23 23:26:55 -0300

    Add /users to urls file

--- app/urls.py
+++ app/urls.py
@@ -1,5 +1,6 @@
 from app import rest_api
-from app.api.resources import Alerts, Tokens
+from app.api.resources import Alerts, Tokens, LoginUsers
 
 rest_api.add_resource(Alerts, "/alerts")
 rest_api.add_resource(Tokens, "/tokens")
+rest_api.add_resource(LoginUsers, "/users")
